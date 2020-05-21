bad5b871067b1370d710a3dde9cd825cd48b7529
javier <javierlopera988@gmail.com>
2020-05-21 19:00:25 +0200

    Arreglando fecha close #13 close #14

--- js/validar.js
+++ js/validar.js
@@ -16,7 +16,7 @@ validar = (function () {
     let expresiones = {
         arrayTexto: [new RegExp("^[a-zA-Zá-úÁ-Ú0-9 ]+$"), "Se debe escribir un texto"],
         arrayNumero: [new RegExp("^[0-9]+$"), "Se debe escribir un número entero"],
-        arrayFecha: [new RegExp("^([0-9]{2})([/-])([0-9]{2})([/-])([0-9]{4})$")],
+        arrayFecha: [new RegExp("^([0-9]{2})([/-])([0-9]{2})\\2([0-9]{4})$"), "Introduce un formato válido, '22/12/2222' o '29-02-2000'"],
         arrayDni: [new RegExp("^([0-9]{8})[- ]?([a-zA-Z])$"), "12345678Z o 12345678-Z", "TRWAGMYFPDXBNJZSQVHLCKET"],
         arrayCorreo: [new RegExp("^[0-9a-zA-Z]+[@][0-9a-zA-Z]+[.][a-zA-Z]+$"), "Se debe poner el correo correctamente"],
         arrayTelefono: [new RegExp("^([+]?[0-9]?[0-9]?[0-9]?[0-9]?)?[ ]?[0-9]{3}[ ]?[0-9]{3}[ ]?[0-9]{3}[ ]?$"), "Número no válido"],
@@ -36,7 +36,7 @@ validar = (function () {
     // Comprueba que el numero sea correcto
 
     let comprobarNumero = function (numero) {
-        if(!expresiones.arrayNumero[0].test(numero.trim())){
+        if (!expresiones.arrayNumero[0].test(numero.trim())) {
             return expresiones.arrayNumero[1];
         }
         return "";
@@ -44,8 +44,8 @@ validar = (function () {
 
     // Comprueba si el checkBox esta activo
 
-    let comprobarCheckbox = function (input) { 
-        if(!input.checked){
+    let comprobarCheckbox = function (input) {
+        if (!input.checked) {
             return "Este campo debe estar seleccionado"
         }
         return "";
@@ -57,58 +57,56 @@ validar = (function () {
         let check = false;
 
         for (const i of opciones) {
-            if(!check && i.checked){
+            if (!check && i.checked) {
                 check = true;
             }
         }
         if (!check) {
             campoError.innerHTML = "Debes seleccionar algún campo";
         }
-        else{
+        else {
             campoError.innerHTML = "";
         }
     }
 
     // Comprueba que el radio Button no quede vacio
 
-    let comprobarSelect = function (opciones,campoError) {
+    let comprobarSelect = function (opciones, campoError) {
         let check = true;
 
         for (const i of opciones) {
-            if(opciones[0].selected){
+            if (opciones[0].selected) {
                 check = false;
             }
         }
         if (!check) {
             campoError.innerHTML = "Debes seleccionar algún campo";
         }
-        else{
+        else {
             campoError.innerHTML = "";
         }
     }
 
-    function comprobarFechaNacimiento (fecha){
+    function comprobarFechaNacimiento(fecha) {
 
-        let fechaDestructuring = expresiones.arrayFecha[0].exec(fecha);
-        
-        if(fecha == ""){
-            return "El campo no puede quedar vacio";
+        try {
+
+            let [, dia, , mes, anno] = expresiones.arrayFecha[0].exec(fecha);
+
+            let date = new Date(`${anno}/${mes}/${dia}`);
+
+            if (Number(dia) != date.getDate() || Number(mes) != date.getMonth() + 1 || Number(anno) != date.getFullYear()) {
+                return "La fecha no es válida";
+            }
+
+            return "";
+
+        } catch (error) {
+            if (fecha.trim() == "") {
+                return "El campo no puede quedar vacio";
+            }
+            return expresiones.arrayFecha[1];
         }
-        if(fechaDestructuring == null){
-            return "Formato incorrecto, 'DD-MM-AAAA'";
-        }
-
-        let date = new Date(`${fechaDestructuring[5]}/${fechaDestructuring[3]}/${fechaDestructuring[1]}`);
-        
-        if(fechaDestructuring[2] != fechaDestructuring[4]){
-            return "El separador debe de ser el mismo para separar dia/mes y mes/año";
-        }
-
-        if(Number(fechaDestructuring[1]) != date.getDate() || Number(fechaDestructuring[3]) != date.getMonth() +1 || Number(fechaDestructuring[5]) != date.getFullYear()){
-            return "La fecha no es válida";
-        }  
-
-        return "";
     }
 
     // Comprueba que el Dni sea correcto
@@ -129,21 +127,21 @@ validar = (function () {
     }
 
     let comprobarCorreo = function (correo) {
-        if(!expresiones.arrayCorreo[0].test(correo.trim())){
+        if (!expresiones.arrayCorreo[0].test(correo.trim())) {
             return expresiones.arrayCorreo[1];
         }
         return "";
     }
 
     let comprobarTelefono = function (telefono) {
-        if(!expresiones.arrayTelefono[0].test(telefono.trim())){
+        if (!expresiones.arrayTelefono[0].test(telefono.trim())) {
             return expresiones.arrayTelefono[1];
         }
-        return "";    
+        return "";
     }
 
     let comprobarUrl = function (url) {
-        if(!expresiones.arrayUrl[0].test(url.trim())){
+        if (!expresiones.arrayUrl[0].test(url.trim())) {
             return expresiones.arrayUrl[1];
         }
         return "";
