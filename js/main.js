b5e0392c091f97761a1791aeb483b177b78df05a
javier <javierlopera988@gmail.com>
2020-05-24 03:48:45 +0200

    close #18

--- js/main.js
+++ js/main.js
@@ -1,53 +1,53 @@
 {
 
-/**
- * Clase de inicio
- * 
- * @author Javier Lopera Jiménez
- * 
-*/
+    /**
+     * Clase de inicio
+     * 
+     * @author Javier Lopera Jiménez
+     * 
+    */
 
 
     // Funciones para chequear
 
-    function chequeaTexto (){
+    function chequeaTexto() {
         this.nextSibling.nextSibling.innerHTML = validar.comprobarTexto(this.value);
     }
 
-    function chequeaNumero (){
+    function chequeaNumero() {
         this.nextSibling.nextSibling.innerHTML = validar.comprobarNumero(this.value);
     }
 
-    function chequeaFecha (){
+    function chequeaFecha() {
         this.nextSibling.nextSibling.innerHTML = validar.comprobarFechaNacimiento(this.value);
     }
 
-    function chequeaDni (){
+    function chequeaDni() {
         this.nextSibling.nextSibling.innerHTML = validar.comprobarDni(this.value);
     }
 
-    function chequeaCorreo (){
+    function chequeaCorreo() {
         this.nextSibling.nextSibling.innerHTML = validar.comprobarCorreo(this.value);
     }
 
-    function chequeaTelefono (){
+    function chequeaTelefono() {
         this.nextSibling.nextSibling.innerHTML = validar.comprobarTelefono(this.value);
     }
 
-    function chequeaUrl (){
+    function chequeaUrl() {
         this.nextSibling.nextSibling.innerHTML = validar.comprobarUrl(this.value);
     }
 
-    function chequeaRadio (){
-        validar.comprobarRadio(document.getElementsByName("radio"),document.getElementById("errorRadioButton"));
+    function chequeaRadio() {
+        validar.comprobarRadio(document.getElementsByName("radio"), document.getElementById("errorRadioButton"));
     }
 
-    function chequeaSelect (){
-        validar.comprobarSelect(document.getElementById("select").value,document.getElementById("errorSelect"));
+    function chequeaSelect() {
+        validar.comprobarSelect(document.getElementById("select").value, document.getElementById("errorSelect"));
         // validar.comprobarSelect(document.getElementById("select"),document.getElementById("errorSelect"));
     }
 
-    function chequeaCheckbox () {
+    function chequeaCheckbox() {
         this.nextSibling.nextSibling.innerHTML = validar.comprobarCheckbox(document.getElementById("checkbox"));
     }
 
@@ -56,7 +56,7 @@
         numero: chequeaNumero,
         correo: chequeaCorreo,
         fecha: chequeaFecha,
-        dni : chequeaDni,
+        dni: chequeaDni,
         telefono: chequeaTelefono,
         url: chequeaUrl,
         checkbox: chequeaCheckbox,
@@ -75,44 +75,40 @@
 
         // Utilizo el for para recorrer todos los elementos del radioButton
         for (const i of document.getElementsByName("radio")) {
-            i.addEventListener("blur",chequeaRadio);
+            i.addEventListener("blur", chequeaRadio);
         }
 
         // Escuchador del select
 
-        document.getElementById("select").addEventListener("blur",chequeaSelect);
+        document.getElementById("select").addEventListener("blur", chequeaSelect);
 
         // Boton de sumbit
 
         document.getElementsByTagName("form")[0].addEventListener("submit", function (event) {
-            
+
             let inputs = document.getElementsByTagName("input");
 
             event.preventDefault();
-            
-            for(input of inputs) {
+
+            for (input of inputs) {
                 input.dispatchEvent(new Event("blur"));
             }
 
             document.getElementById("select").dispatchEvent(new Event("blur"));
-            
+
             for (const i of document.getElementsByTagName("span")) {
-                if(i.innerHTML==""){
-                
-                }
-                else{
-                    if(i.innerHTML!=""){
-                        event.preventDefault();
-                        i.previousSibling.previousSibling.focus();
-                        break;
-                    }
+                if (i.innerHTML != "") {
+
+                    event.preventDefault();
+                    i.previousSibling.previousSibling.focus();
+                    break;
                 }
             }
         });
 
         // Boton de rellenar
 
-        document.getElementById("rellenar").addEventListener("click", function (event){
+        document.getElementById("rellenar").addEventListener("click", function (event) {
             document.getElementById("texto").value = "Hola";
             document.getElementById("numero").value = "98";
             document.getElementById("correo").value = "javiLoji@gmail.com";
@@ -128,7 +124,7 @@
 
         // Boton de vaciar
 
-        document.getElementById("formulario").addEventListener("reset",function (){
+        document.getElementById("formulario").addEventListener("reset", function () {
 
             let spans = document.getElementsByTagName("span");
 
@@ -136,7 +132,7 @@
                 span.innerHTML = "";
             }
         })
-        
+
 
     }
 
